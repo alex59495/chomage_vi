8195b2bc6eb226e1b5d8e246f877c3ab2a76c7d5
maxencelenoir <maxence.lenoir1206@gmail.com>
2020-09-11 15:04:26 +0200

    Changement base de données + création plusieurs jobs

--- app/models/info.rb
+++ app/models/info.rb
@@ -7,7 +7,7 @@ class Info < ApplicationRecord
   has_many :jobs
 
   def unemployment_max_min
-    if (days_unemployment > 730 || days_unemployment < 182) 
+    if days_unemployment.present? && unemployment == "Oui" && (days_unemployment > 730 || days_unemployment < 182) 
       errors.add(:start_unemployment_at, "La durée d'indemnisation est obligatoirement entre 182 et 730 jours")
     end
   end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,21 @@
 class Job < ApplicationRecord
   belongs_to :info
+  validates :company, presence: true
+  validates :work, presence: true
+  validates :start_at, presence: true
+  validates :end_at, presence: true
+  validate :start_at_cannot_be_in_the_future
+  
+  def start_at_cannot_be_in_the_future
+    if start_at.present? && start_at > Date.today 
+      errors.add(:start_at, "ne peut pas être une date future")
+    end
+  end
+
+  def end_at_cannot_be_before_start_at
+    if start_at.present? && end_at.present? && end_at < start_at
+      errors.add(:end_at, "ne peut pas être avant la date de début du contrat")
+    end
+  end
+
 end
